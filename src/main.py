2818073f64e55647375518ab7943247653e5855d
Nicolas Docolas <nicolas.docolas1804@gmail.com>
2025-06-20 23:20:24 -0300

    adding gui

--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 from cli import TicTacToeCLI
-
+from ui.gui import MainWindow
 if __name__ == "__main__":
-    TicTacToeCLI().start()
+    choice = None
+    while choice not in ["1", "2"]:
+        choice = input("""
+Escolha:
+[ 1 ] Terminal
+[ 2 ] GUI
+""")
+        if choice not in ["1", "2"]:
+            print("Opção inválida! Por favor, escolha 1 ou 2.")
+    
+    if choice == "1":
+        TicTacToeCLI().start()
+    elif choice == "2":
+        app = MainWindow()
+        app.mainloop()
